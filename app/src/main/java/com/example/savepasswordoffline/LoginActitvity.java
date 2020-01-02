b623a1962a4eeef860b25a22f6bafa8891ee457d
AshutoshAgarwal <ashutoshagarwal100@gmail.com>
2020-01-02 19:54:24 +0530

    Create Project and create design of Sign-in and Connect with Firebase

---
+++ app/src/main/java/com/example/savepasswordoffline/LoginActitvity.java
@@ -0,0 +1,14 @@
+package com.example.savepasswordoffline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_actitvity);
+    }
+}
